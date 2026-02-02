1ffb7734fa5c647a1a9af38eabfc98dba745a783
nyanshiba <31783332+nyanshiba@users.noreply.github.com>
2026-02-02 16:37:45 +0900

    10秒 or 32パケット or 100秒毎に送ることで、適ピーク利用時の正確性を担保しつつ、適度な粒度のグラフになる

--- loki/sflow-collector.sh
+++ loki/sflow-collector.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 loki_url="http://loki.home.arpa:3100/loki/api/v1/push"
+send_rate=31 # 0で全てのフローサンプルを送る
 if_wan=1 # GigaEthernet0
 ip4_suffix_regex='s/^(\w+.){2}(.*)$/\2/' # 10.0.1.2 -> 1.2
 ip6_suffix_regex='s/^\w+(:\w+){3}(\w.*)$/\2/' # 2001:db8:b0ba:10ee:: -> e::
 
 function sampling_rate_to_factor() {
-  # bytes/s -> bits/10s
-  echo $(($1 * 8 / 10))
+  # bytes/s -> bits/Xs
+  echo $(($1 * 8 / ($2 - $3)))
 }
 
 function addr_to_suffix() {
@@ -26,6 +27,8 @@ function tci_to_vid() {
   echo $(( $1 & 0x0fff ))
 }
 
+cnt=1
+prev_time_received=`date +%s`
 goflow -kafka=false -logfmt=json -nf=false | while read -r line; do
   read -r time_received sampling_rate bytes src_addr dst_addr eth_type in_if out_if src_mac tci iptos ipttl < <(
     echo "$line" | jq -r '[.TimeReceived, .SamplingRate, .Bytes, .SrcAddr, .DstAddr, .Etype, .InIf, .OutIf, .SrcMac, .VlanId, .IPTos, .IPTTL] | @tsv'
@@ -35,7 +38,6 @@ goflow -kafka=false -logfmt=json -nf=false | while read -r line; do
   # 下り
   if [ "$in_if" == "$if_wan" ]; then
     bytes_in=$((bytes_in + bytes))
-    factor_in=`sampling_rate_to_factor $sampling_rate`
     # decimal to hex
     eth_type_in=`printf '0x%x\n' $eth_type`
 
@@ -58,14 +60,12 @@ goflow -kafka=false -logfmt=json -nf=false | while read -r line; do
   # VLAN間 0x3FFFFFFF: IXの自生成パケット
   elif [ "$out_if" != "$if_wan" ] && [ "$out_if" != "1073741823" ]; then
     bytes_local=$((bytes_local + bytes))
-    factor_local=`sampling_rate_to_factor $sampling_rate`
     vid_local=`tci_to_vid $tci`
     src_addr_local=`addr_to_suffix $src_addr $eth_type`
     # echo "DEBUG $in_if $out_if $bytes_local $factor_local $vid_local $src_addr_local"
   # たぶん上り
   else
     bytes_out=$((bytes_out + bytes))
-    factor_out=`sampling_rate_to_factor $sampling_rate`
     vid_out=`tci_to_vid $tci`
     src_addr_out=`addr_to_suffix $src_addr $eth_type $src_mac`
     dst_addr_out="$dst_addr"
@@ -77,43 +77,40 @@ goflow -kafka=false -logfmt=json -nf=false | while read -r line; do
     # echo "DEBUG $in_if $out_if $bytes_out $factor_out $vid_out $src_addr_out $dst_addr_out $iptos_out"
   fi
 
-  # 10秒ごとにLokiに送信 trrafic bits/s = Bytes * 8bits * 2000samples / 10sec
-  # time_received_100s="${time_received%??}00000000000"
-  time_received_10s="${time_received%?}0000000000"
-
-  if [ "$time_received_10s" != "$prev_time_received_10s" ]; then
+  # 10秒 or send_rate or 100秒毎に送る
+  if [ $cnt -gt $send_rate ] && [ "${time_received%?}" != "${prev_time_received%?}" ] || [ "${time_received%??}" != "${prev_time_received%??}" ]; then
+    factor=`sampling_rate_to_factor $sampling_rate $time_received $prev_time_received`
+    # echo "DEBUG $time_received $factor"
 
     payload=$(jq -n \
       --arg bytes_in "$bytes_in" \
-      --arg factor_in "$factor_in" \
+      --arg factor "$factor" \
       --arg eth_type_in "$eth_type_in" \
       --arg src_addr_in "$src_addr_in" \
       --arg dst_addr_in "$dst_addr_in" \
       --arg ipttl_in "$ipttl_in" \
       --arg iptos_in "$iptos_in" \
       --arg bytes_local "$bytes_local" \
-      --arg factor_local "$factor_local" \
       --arg vid_local "$vid_local" \
       --arg src_addr_local "$src_addr_local" \
       --arg bytes_out "$bytes_out" \
-      --arg factor_out "$factor_out" \
       --arg vid_out "$vid_out" \
       --arg src_addr_out "$src_addr_out" \
       --arg dst_addr_out "$dst_addr_out" \
       --arg iptos_out "$iptos_out" \
-      --arg time_received_ns "$time_received_10s" '
+      --arg time_received_ns "${time_received}000000000" '
       {
         "streams": [
           {
-            "stream": { "job": "goflow", "factor": $factor_in, "direction": "in", "eth_type": $eth_type_in, "src_addr": $src_addr_in, "dst_addr": $dst_addr_in, "ipttl": $ipttl_in, "iptos": $iptos_in },
+            "stream": { "job": "goflow", "factor": $factor, "direction": "in", "eth_type": $eth_type_in, "src_addr": $src_addr_in, "dst_addr": $dst_addr_in, "ipttl": $ipttl_in, "iptos": $iptos_in },
             "values": [ [$time_received_ns, $bytes_in] ]
           },
           {
-            "stream": { "job": "goflow", "factor": $factor_local, "direction": "vlan", "vid": $vid_local, "src_addr": $src_addr_local },
+            "stream": { "job": "goflow", "factor": $factor, "direction": "vlan", "vid": $vid_local, "src_addr": $src_addr_local },
             "values": [ [$time_received_ns, $bytes_local] ]
           },
           {
-            "stream": { "job": "goflow", "factor": $factor_out, "direction": "out", "vid": $vid_out, "src_addr": $src_addr_out, "dst_addr": $dst_addr_out, "iptos": $iptos_out },
+            "stream": { "job": "goflow", "factor": $factor, "direction": "out", "vid": $vid_out, "src_addr": $src_addr_out, "dst_addr": $dst_addr_out, "iptos": $iptos_out },
             "values": [ [$time_received_ns, $bytes_out] ]
           }
         ]
@@ -123,24 +120,26 @@ goflow -kafka=false -logfmt=json -nf=false | while read -r line; do
     curl -X POST -H "Content-Type: application/json" -d "$payload" "$loki_url"
     # echo "DEBUG Sent data to Loki: $payload"
 
-    prev_time_received_10s=$time_received_10s
+    cnt=1
+    prev_time_received=$time_received
+    time_received_ns=""
+    factor=""
+
     bytes_in=0
-    factor_in=""
     eth_type_in=""
     src_addr_in=""
     dst_addr_in=""
     ipttl_in=""
     iptos_in=""
     bytes_local=0
-    factor_local=""
     vid_local=""
     src_addr_local=""
     bytes_out=0
-    factor_out=""
     vid_out=""
     src_addr_out=""
     dst_addr_out=""
     iptos_out=""
-    time_received_ns=""
   fi
+
+  ((cnt++))
 done
